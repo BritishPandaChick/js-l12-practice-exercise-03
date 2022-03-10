11bdaa1dfe798de1679b08acc74777c287844590
BritishPandaChick <sbartley1080@gmail.com>
2022-03-09 20:52:18 -0500

    Add comments. Create README

---
+++ README.md
@@ -0,0 +1,11 @@
+# Field Trip Registrations 
+Practice exercise three from Intro to APIs lesson in Skillcrush 102.
+
+## Instructions
+Clone this repo or download the zip file. Open index.html file in your browser to see which students have not registered for the field trip.
+
+## Contributions
+This website is based on a desing created by Skillcrush. Therefore I will not be accepting any pull requests.
+
+## Acknowledgements
+* Skillcrush
--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
+// Global variable 
 const notRegistered = document.querySelector(".not-registered");
 
+// Get registration data
 const getRegistrationData = async function() {
     const request = await fetch("https://gist.githubusercontent.com/skillcrush-curriculum/4bfa96d3762f86d2327d389063b3ea8d/raw/86e50e48e98c662ead6d9bd191ef7db381e9f4b4/fieldtrip.json");
     const data = await request.json();
@@ -7,6 +9,8 @@ const getRegistrationData = async function() {
     displayContactList(data);
 }
 
+
+// Display list 
 const displayContactList = function(data) {
     for (const student of data) {
         if (student.registered === "no") {
